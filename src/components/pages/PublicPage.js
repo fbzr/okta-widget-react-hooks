a24a9fc0e5bad7a5bb0d6a61d201bd6cabee435c
fbzr <fabricio.spb@gmail.com>
2020-05-20 14:28:30 -0700

    add check if user is authenticated in public page

--- src/components/pages/PublicPage.js
+++ src/components/pages/PublicPage.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
+import { useHistory } from 'react-router-dom';
 
 const PublicPage = () => {
-    const { authService } = useOktaAuth();
+    const { push } = useHistory();
+    const { authService, authState } = useOktaAuth();
 
     const login = async () => {
         await authService.login('/private');
     }
-    
+
     return (
         <div>
             <h1>Public Page</h1>
-            <button onClick={login}>Login</button>
+            { authState.isAuthenticated ?
+                <button onClick={() => push('/private')}>Access Private</button>
+                :
+                <button onClick={login}>Login</button>
+            }
         </div>
     )
 }
